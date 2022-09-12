c3c60b74ed6c0e501e9a86ac4d507c0e15c9a394
SONATA\Ramakrishna.P <pradhana.ramakrishna@gmail.com>
2022-09-12 12:35:53 +0530

    daily routine

---
+++ TestOwelOrConsonent.java
@@ -0,0 +1,18 @@
+package javaindebuggingmode;
+
+public class TestOwelOrConsonent {
+	static void Vowel_Or_Consonant(char y)
+	{
+		if(y=='a'||y=='e'||y=='i'||y=='o'||y=='u')
+			System.out.println("It is a Owel");
+		else
+			System.out.println("It is a consonent");
+	}
+	public static void main(String[] args) {
+		Vowel_Or_Consonant('b');
+				Vowel_Or_Consonant('u');
+				
+	}
+	
+
+}
